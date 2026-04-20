b83f6255d8b507d64f07faa761b7575982354552
Mayur Nikam <mayurnikam2580@gmail.com>
2026-04-20 15:19:05 +0530

    modified

--- architecture_graph.py
+++ architecture_graph.py
@@ -7,12 +7,12 @@ import io
 png_bytes = graph.get_graph().draw_mermaid_png()
 
 # Save bytes to file
-with open("my_image.png", "wb") as f:
+with open("architecture_image.png", "wb") as f:
     f.write(png_bytes)
 
 # Or use PIL for more options
 pil_img = PILImage.open(io.BytesIO(png_bytes))
-pil_img.save("my_image.png")
+pil_img.save("architecture_image.png")
 
 # Display
 display(Image(png_bytes))
